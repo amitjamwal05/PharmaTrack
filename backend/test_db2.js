2cb2afc2eca72901f31950d544491a175256ddaf
amitjamwal <bikujamwal@gmail.com>
2026-06-21 17:29:08 +0530

    chore: remove test scripts with sensitive data

--- backend/test_db2.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require('mongodb');
-MongoClient.connect('mongodb://127.0.0.1:27017').then(async client => {
-  const db = client.db('pharmatrack');
-  const bills = await db.collection('bills').find({}).sort({createdAt: -1}).limit(5).toArray();
-  require('fs').writeFileSync('bills.json', JSON.stringify(bills, null, 2));
-  client.close();
-  process.exit(0);
-});
